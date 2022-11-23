aa35cf5e6d17674dc98e7848d883b6fbbb4f9dfc
coloradude <steezeven@gmail.com>
2022-11-22 23:10:27 -0700

    updated version

--- check.js
+++ check.js
@@ -6,15 +6,16 @@ const res = await Spypoint.login('robboflo@gmail.com', 'DahmLake2020!')
 const cameras = await Spypoint.cameras()
 
 const photo = await Spypoint.photosByCamera('62d769b0378d662dc78ed441')
+console.log(photo)
 
 // // const newPhotos = await Spypoint.mostRecentPhotosByCamera()
 // // console.log(newPhotos)
 
-const allPhotos = await Spypoint.queryAllPhotos({ limit: 5, tags: ['deer']})
+// const allPhotos = await Spypoint.queryAllPhotos({ limit: 5, tags: ['deer']})
 
 
 
-console.log(allPhotos)
+// console.log(allPhotos)
 
 
 
